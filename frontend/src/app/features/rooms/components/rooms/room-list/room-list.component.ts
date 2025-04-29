40f5dce4c1d87643baae5615c8f1b0f21a607340
SaTriX <166950004+SaTriX43@users.noreply.github.com>
2025-04-29 15:39:36 -0500

    feat: implemente filtrado por nombre en sala

--- frontend/src/app/features/rooms/components/rooms/room-list/room-list.component.ts
+++ frontend/src/app/features/rooms/components/rooms/room-list/room-list.component.ts
@@ -1,12 +1,12 @@
 // frontend/src/app/features/rooms/components/room-list/room-list.component.ts
 import { Component, OnInit, inject, signal, WritableSignal, Output, EventEmitter } from '@angular/core';
 import { RoomService } from '../../../services/room.service';
-import { SalaCine } from '../../../models/rooms';
+import { SalaCine, SalaCineStatus } from '../../../models/rooms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'room-list-component', 
+  selector: 'room-list-component',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './room-list.component.html',
@@ -18,6 +18,8 @@ export class RoomListComponent implements OnInit {
   loading = signal(true);
   error = signal<any>(null);
 
+  searchTerm = signal('');
+
   @Output() editRoomEvent = new EventEmitter<number>();
 
   constructor() { }
@@ -27,9 +29,22 @@ export class RoomListComponent implements OnInit {
   }
 
   public loadSalas(): void {
-      this.loading.set(true);
-      this.error.set(null);
+    this.loading.set(true);
+    this.error.set(null);
 
+    if (this.searchTerm()) {
+      this.roomService.searchSalasCineByName(this.searchTerm()).subscribe({
+        next: (data) => {
+          this.salas.set(data);
+          this.loading.set(false);
+        },
+        error: (err) => {
+          console.error('Error al buscar salas por nombre:', err);
+          this.error.set(err);
+          this.loading.set(false);
+        }
+      });
+    } else {
       this.roomService.getAllSalasCine()
       .subscribe({
         next: (data) => {
@@ -42,10 +57,37 @@ export class RoomListComponent implements OnInit {
           this.loading.set(false);
         }
       });
+    }
+  }
+
+  onSearchInputChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    this.searchTerm.set(inputElement.value);
+  }
+
+  onSearchClick(): void {
+    this.loadSalas();
+  }
+
+  clearSearch(): void {
+    this.searchTerm.set('');
+    this.loadSalas();
   }
 
   onEditClick(id: number): void {
-      this.editRoomEvent.emit(id);
+    this.editRoomEvent.emit(id);
   }
 
+  onViewStatusClick(id: number): void {
+    console.log('Obteniendo estado para sala con ID:', id);
+    this.roomService.getSalaCineStatus(id).subscribe({
+      next: (statusData) => {
+        console.log('Estado Calculado:', statusData.estado_calculado);
+        alert(`Estado Calculado para Sala ${statusData.nombre}: ${statusData.estado_calculado}`);
+      },
+      error: (err) => {
+        console.error('Error al obtener estado de la sala:', err);
+      }
+    });
+  }
 }
